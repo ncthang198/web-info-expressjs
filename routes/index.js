8d34b9ac58ee1a63797f0ac752f65dabcbca7ee0
Thang Nguyen <ncthang198@gmail.com>
2020-11-30 22:53:34 +0700

    b

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   const oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
-  const firstDate = new Date(2008, 1, 12);
-  const secondDate = new Date(2008, 1, 22);
+  const firstDate = new Date(2020, 6, 6);
+  const secondDate = new Date(2020,11,30);
 
   const diffDays = Math.round(Math.abs((firstDate - secondDate) / oneDay));
   console.log(diffDays)
